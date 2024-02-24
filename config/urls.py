071f775a6c30bad028604c53edc53dd496707db6
Sulaymonqori <ms13061312@gmail.com>
2024-02-24 16:43:30 +0500

    acoounts linked

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from config.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing_page'),
+    path('gap/', include('apps.gap.urls')),
+    path('accounts/', include('apps.accounts.urls')),
 ]
