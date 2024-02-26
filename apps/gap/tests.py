45db2ff4e5f1caa26a9bd5fd4be3608f7a835574
Sulaymonqori <ms13061312@gmail.com>
2024-02-26 13:00:31 +0500

    test for opinion list

--- apps/gap/tests.py
+++ apps/gap/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from apps.gap.models import Opinion, Room
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class OptionTestCase(TestCase):
+    def SetUp(self):
+        user = User.objects.create(username="testuser")
+        user.set_password("testpass")
+        user.save()
+        self.user = user
+
+        room = Room.objects.create(name="test")
+        room.save()
+        self.room = room
+
+        opinion = Opinion.objects.create(
+            room=self.room,
+            author=self.user,
+            title="Lorem ipsum dolor sit amet",
+            body="opinion body"
+        )
+        opinion.save()
+        self.opinion = opinion
+
+    def test_opinion_list(self):
+        response = self.client.get("landing_page")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Lorem ipsum dolor sit amet")
+
+    # har doim 404 status code qaytdi negaligini hech bilolmadim ((
